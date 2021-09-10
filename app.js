65a462858baff7dbff1d921f9363a22940507c10
blankwriter <prince.eworho@amalitech.org>
2021-09-10 11:17:48 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//declaring the variable myAge
+const myAge = 24;
+//declaring the variable earlyYears.
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+//declaring the variable laterYears and initilizing it.
+let laterYears = myAge - 2;
+//multiplying laterYears by 4 to calculate the number of dog years
+laterYears = laterYears *4;
+// Declaring the variable myAgrInDogYears and calculating my age in dog years by adding earlyYears to laterYears
+let myAgeInDogYears;
+myAgeInDogYears = earlyYears + laterYears;
+//declaring myName and using the toLowerCase method to return a string with lower case.
+myName='Prince John'.toLowerCase();
+//displaying myName, myAge and the age in dog years.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+
